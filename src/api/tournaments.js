98a5a3a9f5307a38b012fc18e6b9e37341c1658c
Bastian Tangedal <bastian.tangedal@gmail.com>
2020-09-27 13:32:44 +0200

    Initial commit

--- src/api/tournaments.js
+++ src/api/tournaments.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const monk = require('monk');
+const joi = require('joi');
+const Joi = require('joi');
 
 const db = monk(process.env.MONGO_URI);
 const tournaments = db.get('tournaments');
+
+const schema = Joi.object({
+  name: Joi.string().trim().required(),
+  description: Joi.string().trim().required(),
+  place: Joi.string().trim().required(),
+  date: Joi.string().trim().required(),
+  organizer: Joi.string().trim().required(),
+  arbitor: Joi.string().trim().required(),
+  price: Joi.number(),
+})
 
 const router = express.Router();
 
 // Get all tournaments (READ ALL)
 router.get('/', (req, res, next) => {
   try {
-    const allTournaments = tournaments.find({});
-    res.json(allTournaments);
+    tournaments.find({}).then((data) => {
+      res.json(data);
+    })
   } catch (error) {
     next(error);
   }
 });
 
 // Create a tournament (CREATE ONE)
-router.post('/:id', (req, res, next) => {
-  res.json({
-    action: 'Create a tournament',
-    response: req.body,
-  });
+router.post('/:id', async (req, res, next) => {
+  try {
+    console.log(req.body);
+
+    const value = await schema.validateAsync(req.body);
+    const inserted = await tournaments.insert(value)
+
+    res.json(inserted);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Get specific tournament (READ ONE)
