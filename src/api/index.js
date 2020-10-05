66d60726b38d38fcf147970fe265b0e96704e00f
Bastian Tangedal <bastian.Tangedal@gmail.com>
2020-10-05 15:48:44 +0200

    i dunno

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const emojis = require('./emojis');
 const tournaments = require('./tournaments');
+const auth = require('./auth');
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.get('/', (req, res) => {
 
 router.use('/emojis', emojis);
 router.use('/tournaments', tournaments);
+router.use('/user', auth);
 
 module.exports = router;
